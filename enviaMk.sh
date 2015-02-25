e1fd2e7f25e5d9341ea0ce868bd512c097a8ad4f
Alexsandro Felix <felix@ffelix.eti.br>
2015-02-25 10:05:08 -0300

    Update enviaMk.sh

--- enviaMk.sh
+++ enviaMk.sh
@@ -5,42 +5,41 @@
 # Backup of terminal setup
 bkpterminal=`stty -g`
 
-echo "Type your login'"
-read user
-export user=$user
+echo "Type your user:"
+read usuario
+export usuario=$usuario
 
-echo "Type your password"
+echo "Enter your password"
 # Disable the display of characteres for security reasons
 stty -echo intr '^a'
 read passwd
-export pass=$passwd
+export senha=$passwd
 
 # Restore the previously terminal setup
 stty $bkpterminal
 
-export port=64322
+export porta=64322
 
 results=$(mysql --host="$dbhost" --user="$user" --password="$password" --database="$database" --skip-column-names --execute="SELECT ip FROM hotspot;")
 
 for i in $results; do
 	ping -c 1 $i > /dev/null
 	if [ $? = 1 ]; then
-		echo "$i OFFLINE in $(date)" 
+		echo "$i OFFLINE em $(date)" 
 	else
-		echo "$i ONLINE in $(date)" 
+		echo "$i ONLINE em $(date)" 
 
 		# Print user list
-		# sshpass -p $pass ssh $user@$i -p $port /user print ;
+		# sshpass -p $senha ssh $usuario@$i -p $porta /user print ;
 
 		# Print information about the RB
-		# sshpass -p $pass ssh $user@$i -p $port /system resource print
-		sshpass -p $pass ssh $user@$i -p $port /system identity print ;
+		# sshpass -p $senha ssh $usuario@$i -p $porta /system resource print
+		sshpass -p $senha ssh $usuario@$i -p $porta /system identity print ;
 
 
 		# Send update file ans reboot
-		# sshpass -p $pass scp -P $port ~/Downloads/Mikrotik/4xx-7xx/routeros-mipsbe-6.27.npk $user@$i:/
-		# sshpass -p $pass ssh $user@$i -p $port /system reboot ;
-
+		# sshpass -p $senha scp -P $porta ~/Downloads/Mikrotik/4xx-7xx/routeros-mipsbe-6.27.npk $usuario@$i:/
+		# sshpass -p $senha ssh $usuario@$i -p $porta /system reboot ;
 
 
 	fi
